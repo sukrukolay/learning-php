7c5b5c9fa8e7c9cf2eb79f5c6d5e26ef10cb206f
sukrukolay <sukru.kolay1907@gmail.com>
2022-04-03 13:22:39 +0300

    string birlestirme

--- variables/variable.php
+++ variables/variable.php
@@ -7,12 +7,27 @@ echo $txt;
 
 $int = 5;
 echo "Bu bir int sayidir:" .$int; 
-echo " <br/>Alt satir" .$txt;
+echo " <br/>Alt satir" .$txt; //üstteki satırın altına inip $txt değerini yazdırır.
 
 
 $number1 = 1;
 $number2 = 3;
 $result = $number1+ $number2; // result = 1+3
 echo "$number1 + $number2 =".$result; // 1+3 = 4
+echo "<br>";
+
+$ad = "Şükrü";
+$soyad = "Kolay";
+$dogumYili = "20.09.2000";
+
+echo "Adım Soyadım: ".$ad." ".$soyad; echo "<br>";
+echo "Doğum Tarihim: "  ."$dogumYili";
+
+/*
+$ad + $soyad yazılırsa hata verir.
+. operatörü birleştirmek için kullanılır.
+*/
+
+
 
 ?> 
