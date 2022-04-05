17cfa4a081ff818240f67a991ae7118754d62109
sukrukolay <sukru.kolay1907@gmail.com>
2022-04-05 17:20:21 +0300

    foreach-loop

--- loops/foreach.php
+++ loops/foreach.php
@@ -10,9 +10,17 @@ foreach($names as $iter) // foreach($hangi_dizi_üzerinde_işlem_yapılacak   as
     echo "<br>";
 }
 echo "<br> <br>";
-foreach($names as $index => $name) 
+foreach($names as $index => $name)  // $index=>$name demek index ve değeri ifade eder. $names[0]=ali gibi
 {
     echo "$index ---> $name";
     echo "<br>";
 }
+/*
+çıktı:
+0--->ali
+1--->burak
+...
+..
+.
+*/
 ?>
