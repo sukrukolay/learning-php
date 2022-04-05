d6763d806f1fc2632d3b005dc531b34ce8af8267
sukrukolay <sukru.kolay1907@gmail.com>
2022-04-05 16:17:16 +0300

    if-elseif-else

--- conditional-statements/if-statements.php
+++ conditional-statements/if-statements.php
@@ -1,25 +1,25 @@
 <?php
 $not = 73;
-if($not <"50")
+if($not <"50") //$not 50den küçükse kaldiniz yazdir.
 {
     echo "Kaldiniz";
 }
-else if($not > 50 && $not <80){
+else if($not > 50 && $not <80){ //$not 50 den büyük ve 80 den küçükse geçtiniz yazdir.
     echo "Geçtiniz";
 }
-else if($not >=80 && $not <=100){
+else if($not >=80 && $not <=100){ //$not 80 den büyük eşit ve 100 den küçük eşitse mükemmel yazdir.
     echo "Mükemmel";
 }
 
 echo "<br>";
-$saat = date("H");
-if($saat < 10){
+$saat = date("H"); //$saat değerine şu anki saat değerini at.
+if($saat < 10){ // $saat 10 dan küçükse sabah yazdir.
     echo "sabah";
 }
-else if($saat > 10 && $saat <19 ){
-    echo "öğlen";
+else if($saat > 10 && $saat <16 ){ // $saat 10 dan büyükse ve 19 dan küçükse öğle yazdir.
+    echo "öğle";
 }
-else{
-    echo "gece";
+else{ // üstteki if lerden hiçbirisine uymuyorsa akşam yazdir.
+    echo "akşam";
 }
 ?>
