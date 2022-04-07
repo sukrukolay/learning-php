d737203193ae13b76f68fa576f686da7dd77d479
sukrukolay <sukru.kolay1907@gmail.com>
2022-04-07 18:27:19 +0300

    request

---
+++ superglobals/request-sonuc.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+    $_REQUEST: URL üzerinden veya istek gönderim tag'ları aracılığıyla gelen verilere
+ulaşabilme imkanı tanır. Genellikle URL üzerinden gelen istekler, kapalı yöntemle gelen
+istekler ve form işleme işlemleri için kullanılmaktadır.
+GET VE POST' un yaptıklarını tek başına yapabilir.
+*/
+
+    $gelenAd    = $_REQUEST["isim"];
+    $gelenSoyad = $_REQUEST["soyIsim"]; 
+    echo $gelenAd." " .$gelenSoyad;
+?>
