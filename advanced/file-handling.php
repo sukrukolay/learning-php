393a45b4b01a51e5768b687ddf06de09cf8db644
sukrukolay <sukru.kolay1907@gmail.com>
2022-04-08 16:24:18 +0300

    file-handling

---
+++ advanced/file-handling.php
@@ -0,0 +1,5 @@
+<?php
+
+    $d = readfile("a.txt"); // a.txt adından bulunan dosyayı oku ve $d değişkenine aktar.
+    echo $d; // okunan dosyanın içeriğini $d değişkenine aktarmıştık. Bu satırda o metni yazdır.
+?>
