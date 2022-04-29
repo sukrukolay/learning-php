b5e50ac424d4a2b8523c1ec55dbd6b8f5d82df69
sukrukolay <sukru.kolay1907@gmail.com>
2022-04-29 09:12:11 +0300

    private

---
+++ OOP/Visibility/private.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        // class içinde private olan bir özelliğe erişmek istiyorsak public bir metod tanımlayıp onun vasıtasıyla erişebiliriz. Aşağıdaki örnekte olduğu gibi.
+        class Bir
+        {
+            private $Isim            = "Şükrü";
+            private const SOYISIM    = "Kolay";
+
+            public function Bilgi()
+            {
+                return "Adı Soyadı: ".$this->Isim." ".self::SOYISIM;
+            }
+        }
+
+        $Nesne = new Bir;
+        echo $Nesne->Bilgi(); // Ekrana Adı Soyadı: Şükrü Kolay yazar.
+    ?>
+</body>
+</html>
