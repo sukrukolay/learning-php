1a5b980f06d573b550281ae903e0cab7bbbbb9cb
sukrukolay <sukru.kolay1907@gmail.com>
2022-05-06 11:09:39 +0300

    namespace kullanmadan aynı class'ı kullanmak

---
+++ OOP/namespace/ornek1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    /*
+    namespace : class'lar için özel isim alanları / küme tanımlamak için kullanılır.
+    */
+    
+    class Deneme{ //hata verir çünkü deneme sınıfı birden fazla kez kullanılmıştır.
+
+    }
+
+    class Deneme{
+        
+    }
+    ?>
+</body>
+</html>
