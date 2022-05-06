ecaf2399bf9b1efccdee62d50add65bef311cd6b
sukrukolay <sukru.kolay1907@gmail.com>
2022-05-06 13:49:33 +0300

    namespace hatalı kullanım

---
+++ OOP/namespace/ornek4/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    namespace Uyeler\Sukru; // Hata verir. namespace dökümanın başında kullanılmalıdır.
+        class Deneme{
+
+
+        }
+    
+    namespace Odemeler\KrediKarti; // Hata verir. namespace dökümanın başında kullanılmalıdır.
+        class Deneme{
+
+
+        }
+    ?>
+</body>
+</html>
