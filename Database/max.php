887ba25168a5105a63e843515376bd1ae3090b8b
sukrukolay <sukru.kolay1907@gmail.com>
2022-05-20 17:48:52 +0300

    MAX() fonksiyonunun kullanımı

---
+++ Database/max.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*
+    MIN   = MySQL sunucusundaki database içerisinde bulunan herhangi bir tablonun belirtilecek olan sütununun en küçük değerini bulmak için kullanılır.
+    */
+
+    try{
+        $VeriTabaniBaglantisi   = new PDO("mysql:host=localhost;dbname=extraegitim;charset=UTF8","root","");
+    }catch(PDOException $Hata){
+        echo "Bağlantı Hatası<br/>";
+        echo "Hata Açıklaması : ".$Hata->getMessage();
+        die();
+    }
+
+    $Sorgu  = $VeriTabaniBaglantisi->query("SELECT MAX(yas) as EnBuyukKisininYasi FROM kayitlar");
+    if($Sorgu){
+        $KayitSayisi    = $Sorgu->rowCount();
+        if($KayitSayisi>0){
+            $Kayit    = $Sorgu->fetch();
+            echo "Siteye Kayıtlı En Büyük Kişinin Yaşı : ".$Kayit["EnBuyukKisininYasi"];
+        }
+        else{
+            echo "Kayıt Yok<br/>";
+        }
+    }
+    else{
+        echo "Sorgu Hatası<br/>";
+    }
+    $VeriTabaniBaglantisi   = null;
+    ?>
+</body>
+</html>
