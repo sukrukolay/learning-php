844095255e6c67eb5c5a24ab8ad15f8b28d4a7d0
sukrukolay <sukru.kolay1907@gmail.com>
2022-05-20 11:41:45 +0300

    where ile BETWEEN kullanımı

---
+++ Database/where4.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        /*
+        WHERE   : MySQL suncusundaki database içerisinde bulunan herhangi bir tablonun işlem sırasında tüm verilerini işlemek yerine sadece koşula bağlı verilerin işlemek istenildiğini belirtmek için kullanılır. 
+        */
+
+        try {
+            $VeriTabaniBaglantisi   = new PDO("mysql:host=localhost;dbname=extraegitim;charset=UTF8","root","");
+        } catch (PDOException $th) {
+            echo "Hata Açıklaması : ".$th->getMessage();
+            die();
+        }
+
+        // yas değeri 38 ve 40 arasında olMAYAN kayıtları getir.
+        //$Sorgu  = $VeriTabaniBaglantisi->query("SELECT *FROM kayitlar WHERE yas NOT BETWEEN 38 AND 40" , PDO::FETCH_ASSOC);
+
+
+        // yas değeri 38 ve 40 arasında olan kayıtları getir.
+        $Sorgu  = $VeriTabaniBaglantisi->query("SELECT *FROM kayitlar WHERE yas BETWEEN 38 AND 40" , PDO::FETCH_ASSOC);
+        if($Sorgu){
+            foreach($Sorgu as $Satirlar){
+                echo $Satirlar["id"]." | ". $Satirlar["adisoyadi"] . " | " . $Satirlar["yas"] . " | " .  $Satirlar["sehir"] ."<br/><br/>";
+            }
+        }
+        else{
+            echo "Sorgu Hatası<br/>";
+        }
+        $VeriTabaniBaglantisi    = null;
+
+        /*
+        Ekran Çıktısı
+
+            
+        */
+    ?>
+</body>
+</html>
