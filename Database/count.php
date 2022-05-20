4f4fdc1d659a81676442b962b2e7c064ad841365
sukrukolay <sukru.kolay1907@gmail.com>
2022-05-20 17:19:37 +0300

    COUNT() fonksiyonu kullanımı

---
+++ Database/count.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*
+    COUNT   = MySQL sunucusundaki database içerisinde bulunan herhangi bir tablonun belirtilecek olan sütununun toplam kayıt sayısını bulmak için kullanılır.
+    */
+
+    try{
+        $VeriTabaniBaglantisi   = new PDO("mysql:host=localhost;dbname=extraegitim;charset=UTF8","root","");
+    }catch(PDOException $Hata){
+        echo "Bağlantı Hatası<br/>";
+        echo "Hata Açıklaması : ".$Hata->getMessage();
+        die();
+    }
+
+    $Sorgu  = $VeriTabaniBaglantisi->query("SELECT COUNT(id) as KisiSayisi FROM kayitlar");
+    if($Sorgu){
+        $KayitSayisi    = $Sorgu->fetch();
+        echo "Toplam Kayıt Sayısı : ".$KayitSayisi["KisiSayisi"];
+    }
+    else{
+        echo "Sorgu Hatası<br/>";
+    }
+    $VeriTabaniBaglantisi   = null;
+    ?>
+</body>
+</html>
